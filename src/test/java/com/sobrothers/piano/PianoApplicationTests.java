97c352b76d9b1389bb197a35887c0f06a78f8016
rumor1993 <rumor1993@naver.com>
2021-11-06 11:25:46 +0900

    With Piano Project

---
+++ src/test/java/com/sobrothers/piano/PianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobrothers.piano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
