1860e14c145629928392a00a1122649c59dd0c0d
rumor1993 <rumor1993@naver.com>
2021-11-08 02:25:21 +0900

    시작

--- src/main/java/com/sobrothers/piano/domain/student/web/StudentController.java
+++ src/main/java/com/sobrothers/piano/domain/student/web/StudentController.java
@@ -1,4 +1,27 @@
 package com.sobrothers.piano.domain.student.web;
 
+import com.sobrothers.piano.domain.student.domain.Student;
+import com.sobrothers.piano.domain.student.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+@RequiredArgsConstructor //생성자 주입
+@RestController
 public class StudentController {
+
+    private StudentService studentService;
+
+    @GetMapping("/students")
+    public Student test() throws Exception{
+        Student student = new Student();
+        student.setName("소준혁");
+
+
+
+        return student;
+    }
+
+
 }
